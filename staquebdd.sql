0510a563e4ac2d5dc3f945009637b694fe56e897
Calimero <florianseailles@gmail.com>
2014-10-17 09:51:50 +0200

    bdd

---
+++ staquebdd.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 17 Octobre 2014 à 09:46
+-- Version du serveur :  5.6.16
+-- Version de PHP :  5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `staque`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `foreign_id` int(11) NOT NULL,
+  `foreign_table` enum('question','reponse') NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `contenu` text NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateModified` datetime NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `hasvoted`
+--
+
+CREATE TABLE IF NOT EXISTS `hasvoted` (
+  `user_id` int(11) NOT NULL,
+  `reponse_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `quest`
+--
+
+CREATE TABLE IF NOT EXISTS `quest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(200) NOT NULL,
+  `contenu` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `scorequest` int(11) NOT NULL,
+  `vues` int(11) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateModified` datetime NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rep`
+--
+
+CREATE TABLE IF NOT EXISTS `rep` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `quest_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `contenu` text NOT NULL,
+  `scoreRep` int(11) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateModified` datetime NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  `best` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tag`
+--
+
+CREATE TABLE IF NOT EXISTS `tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tagname` varchar(50) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tag_quest`
+--
+
+CREATE TABLE IF NOT EXISTS `tag_quest` (
+  `tag_id` int(11) NOT NULL,
+  `tag_question` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(50) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(50) NOT NULL,
+  `token` varchar(50) NOT NULL,
+  `photo` varchar(100) NOT NULL,
+  `pays` varchar(50) NOT NULL,
+  `langue` varchar(50) NOT NULL,
+  `metier` varchar(100) NOT NULL,
+  `lien` varchar(250) NOT NULL,
+  `score` int(11) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateModified` datetime NOT NULL,
+  `dateLogged` datetime NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
