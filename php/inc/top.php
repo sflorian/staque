5daf88b68a5edee08fa2ac8dcd678f515fe3da1d
sflorian <florianseailles@gmail.com>
2014-10-21 20:12:52 +0200

    Syntaxhighlighter complete

--- php/inc/top.php
+++ php/inc/top.php
@@ -24,64 +24,64 @@
 		<!-- <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.css" /> -->
 		<!-- JQUERY MOBILE -->
 
-		<script src="../js/syntaxhighlighter/scripts/shCore.js"></script>
-		<link href="../js/syntaxhighlighter/styles/shCoreDefault.css"/>
 		<link href="../js/syntaxhighlighter/styles/shThemeDefault.css"/>
-		<!-- Themes SyntaxHighlighter -->
-		
+		<link href="../js/syntaxhighlighter/styles/shCoreDefault.css"/>
+		<script src="../js/syntaxhighlighter/scripts/shCore.js"></script>
+		<!-- Themes SyntaxHighlighter -->		
 
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushAutoloader.js"></script>
+		<script src="../js/syntaxhighlighterscriptsshBrushAutoloader.js"></script>
 		<!-- SyntaxHighlighter pour Autoloader -->
+		<script src="../js/syntaxhighlighterscriptsshBrushAppleScript.js"></script>
+		<!-- SyntaxHighlighter pour AppleScript -->
+		<script src="../js/syntaxhighlighterscriptsshBrushAS3.js"></script>
+		<!-- SyntaxHighlighter pour AS3 -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushBash.js"></script>
+		<!-- SyntaxHighlighter pour Bash -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushColdFusion.js"></script>
+		<!-- SyntaxHighlighter pour ColdFusion -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushCpp.js"></script>
+		<!-- SyntaxHighlighter pour Cpp -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushCSharp.js"></script>
+		<!-- SyntaxHighlighter pour CSharp -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushCss.js"></script>
+		<!-- SyntaxHighlighter pour Css -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushDelphi.js"></script>
+		<!-- SyntaxHighlighter pour Delphi -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushDiff.js"></script>
+		<!-- SyntaxHighlighter pour Diff -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushErlang.js"></script>
+		<!-- SyntaxHighlighter pour Erlang -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushGroovy.js"></script>
+		<!-- SyntaxHighlighter pour Groovy -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushJava.js"></script>
+		<!-- SyntaxHighlighter pour Java -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushJavaFX.js"></script>
+		<!-- SyntaxHighlighter pour JavaFX -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
+		<!-- SyntaxHighlighter pour JScript -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushPerl.js"></script>
+		<!-- SyntaxHighlighter pour Perl -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushPhp.js"></script>
+		<!-- SyntaxHighlighter pour Php -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushPlain.js"></script>
+		<!-- SyntaxHighlighter pour Plain -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushPowershell.js"></script>
+		<!-- SyntaxHighlighter pour Powershell -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushPython.js"></script>
+		<!-- SyntaxHighlighter pour Python -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushRuby.js"></script>
+		<!-- SyntaxHighlighter pour Ruby -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushSass.js"></script>
+		<!-- SyntaxHighlighter pour Sass -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushScala.js"></script>
+		<!-- SyntaxHighlighter pour Scala -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushSql.js"></script>
+		<!-- SyntaxHighlighter pour Sql -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushVb.js"></script>
+		<!-- SyntaxHighlighter pour Vb -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushXml.js"></script>
+		<!-- SyntaxHighlighter pour Xml -->
 
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		<script type="text/javascript" src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
-		<!-- SyntaxHighlighter pour javascript -->
-
-		
-		<script type="text/javascript">SyntaxHighlighter.all();</script>
+				<script>SyntaxHighlighter.all();</script>
 		<!-- Lancement de syntaxhighlighter -->
 	</head>
