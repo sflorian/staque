b64aec41d879561fcae14eff06a789917b6f7232
Calimero <florianseailles@gmail.com>
2014-10-21 11:00:42 +0200

    profil finish
    profil display works

--- php/pages/profil.php
+++ php/pages/profil.php
@@ -1,31 +1,50 @@
+<?php
+
+	if(!empty($_GET['id'])) {
+		$id = $_GET['id'];
+	$sql = "SELECT * FROM utilisateur
+			WHERE id = :id";
+	$stmt = $dbh->prepare( $sql ); 
+	$stmt->bindValue(":id", $id);
+	$stmt->execute();
+	$utilisateur = $stmt->fetch();
+	}
+?>
+
+
+
+
+
+
 		<main id="mainPoserQuestion">
 			<container>
-				<h1><?= '$pseudo';?></h1>
+				<h1><?= $utilisateur['pseudo'];?></h1>
 
 				<div>
 					<div id="containerPhoto" class="left">
-						<img src="../img/uploads/8-mile-2002.jpg<?php // '$photo';?>" id="profilePic">
-						<a href="#score"><?= '$score';?><br>score</a>
+						<img src="../img/uploads/<?= $utilisateur['photo'];?>" id="profilePic">
+						<a href="#score"><?= $utilisateur['score'];?><br>score</a>
 					</div>
 					<div id="containerInfo" class="left">
 						<div class="labelOfLabel">bio
-							<div class="infoLabel">site : <?= '$lien';?></div>
-							<div class="infoLabel">pays : <?= '$pays';?></div>
-							<div class="infoLabel">age : <?= '$age';?></div>
+							<div class="infoLabel">langue : <?= $utilisateur['langue'];?></div>
+							<div class="infoLabel">pays : <?= $utilisateur['pays'];?></div>
+							<div class="infoLabel">langue : <?= $utilisateur['metier'];?></div>
+							<div class="infoLabel">site : <?= $utilisateur['lien'];?></div>
 						</div>
 						
 						<div class="labelOfLabel">visites
-							<div class="infoLabel">date d'inscription : <?= '$dateCreated';?></div>
-							<div class="infoLabel">dernière visite : <?= '$dateLogged';?></div>
+							<div class="infoLabel">date d'inscription : <?= dateFr($utilisateur['dateCreated']);?></div>
+							<div class="infoLabel">dernière visite : <?= dateFr($utilisateur['dateLogged']);?></div>
 						</div class="labelOfLabel">
 						
-						<div class="labelOfLabel">statistiques
+						<!-- <div class="labelOfLabel">statistiques
 							<div class="infoLabel">vues du profil</div>
-						</div>
+						</div> -->
 						
 						<div class="labelOfLabel">privé
-							<div class="infoLabel">email : <?= '$email';?></div>
-							<div class="infoLabel">nom et prénom : <span><?= '$prenom';?> <?='$nom';?></div>
+							<div class="infoLabel">email : <?= $utilisateur['email'];?></div>
+							<div class="infoLabel">nom et prénom : <span><?= $utilisateur['prenom'];?> <?=$utilisateur['nom'];?></div>
 						</div>
 
 					</div>
