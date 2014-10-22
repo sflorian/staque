5aebc2ad7bea6350f3859480b31fbc8f83bc1fe8
mc <sweetformation@gmail.com>
2014-10-22 09:40:57 +0200

    script ok

--- php/fonctions_redirection.php
+++ php/fonctions_redirection.php
@@ -3,7 +3,7 @@
 
 
 		// Fonction qui redirige vers l'accueil
-	function goHome($message) {
+	function goHome($message = "") {
 		header("Location: ?page=accueil" . $message);
 		die(); 
 	}
@@ -18,7 +18,7 @@
 		// Fonction qui redirige vers l'accueil si l'utilisateur veut accéder à une page sensible sans être connecté
 	function forbidden(){
 		if(!userIsLogged()){
-		$interdit = true;
-		die("VOUS N'AVEZ PAS LE DROIT D'ACCÉDER À CETTE PAGE SANS ÊTRE CONNECTÉ À CE COMPTE!<br>VOUS SEREZ REDIRIGÉ VERS LA PAGE D'ACCUEIL DANS 10 SECONDES.");
+			$interdit = true;
+			die("VOUS N'AVEZ PAS LE DROIT D'ACCÉDER À CETTE PAGE SANS ÊTRE CONNECTÉ À CE COMPTE!<br>VOUS SEREZ REDIRIGÉ VERS LA PAGE D'ACCUEIL DANS 10 SECONDES.");
 		}
 	}
--- php/inc/script.php
+++ php/inc/script.php
@@ -29,5 +29,65 @@
 		<script src="../js/app.js"></script>
 		<script src="../js/fonction.js"></script>
 		<noscript>Your browser does not support JavaScript! | Votre navigateur ne supporte pas Javascript!</noscript>
+
+		<script src="../js/syntaxhighlighter/scripts/shCore.js"></script>
+		<!-- Themes SyntaxHighlighter -->		
+
+		<script src="../js/syntaxhighlighterscriptsshBrushAutoloader.js"></script>
+		<!-- SyntaxHighlighter pour Autoloader -->
+		<script src="../js/syntaxhighlighterscriptsshBrushAppleScript.js"></script>
+		<!-- SyntaxHighlighter pour AppleScript -->
+		<script src="../js/syntaxhighlighterscriptsshBrushAS3.js"></script>
+		<!-- SyntaxHighlighter pour AS3 -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushBash.js"></script>
+		<!-- SyntaxHighlighter pour Bash -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushColdFusion.js"></script>
+		<!-- SyntaxHighlighter pour ColdFusion -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushCpp.js"></script>
+		<!-- SyntaxHighlighter pour Cpp -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushCSharp.js"></script>
+		<!-- SyntaxHighlighter pour CSharp -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushCss.js"></script>
+		<!-- SyntaxHighlighter pour Css -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushDelphi.js"></script>
+		<!-- SyntaxHighlighter pour Delphi -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushDiff.js"></script>
+		<!-- SyntaxHighlighter pour Diff -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushErlang.js"></script>
+		<!-- SyntaxHighlighter pour Erlang -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushGroovy.js"></script>
+		<!-- SyntaxHighlighter pour Groovy -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushJava.js"></script>
+		<!-- SyntaxHighlighter pour Java -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushJavaFX.js"></script>
+		<!-- SyntaxHighlighter pour JavaFX -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushJScript.js"></script>
+		<!-- SyntaxHighlighter pour JScript -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushPerl.js"></script>
+		<!-- SyntaxHighlighter pour Perl -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushPhp.js"></script>
+		<!-- SyntaxHighlighter pour Php -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushPlain.js"></script>
+		<!-- SyntaxHighlighter pour Plain -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushPowershell.js"></script>
+		<!-- SyntaxHighlighter pour Powershell -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushPython.js"></script>
+		<!-- SyntaxHighlighter pour Python -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushRuby.js"></script>
+		<!-- SyntaxHighlighter pour Ruby -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushSass.js"></script>
+		<!-- SyntaxHighlighter pour Sass -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushScala.js"></script>
+		<!-- SyntaxHighlighter pour Scala -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushSql.js"></script>
+		<!-- SyntaxHighlighter pour Sql -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushVb.js"></script>
+		<!-- SyntaxHighlighter pour Vb -->
+		<script src="../js/syntaxhighlighter/scripts/shBrushXml.js"></script>
+		<!-- SyntaxHighlighter pour Xml -->
+
+		<script>SyntaxHighlighter.all();</script>
+		<!-- Lancement de syntaxhighlighter -->
+		
 	</body>
 </html>
