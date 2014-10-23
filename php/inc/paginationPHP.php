a70f262cf37f44e30caf71eb4ecfa08d26d65091
mc <sweetformation@gmail.com>
2014-10-23 12:41:21 +0200

    modif accueil

--- php/inc/paginationPHP.php
+++ php/inc/paginationPHP.php
@@ -1,7 +1,7 @@
 <?php
 	
 
-	$numPerPage = 2;
+	$numPerPage = 3;
 	$p = 1;
 	
 	if (!empty($_GET['p'])){
@@ -27,11 +27,11 @@
 			quest.scorequest AS scorequest, 
 			quest.dateModified AS dateModified, 
 			quest.dateCreated AS 	dateCreated, 
-			rep.dateModified AS repDateModified, 
+			/*rep.dateModified AS repDateModified,*/ 
 			utilisateur.score AS utilisateurScore, 
 			utilisateur.pseudo AS utilisateurPseudo 
 			FROM quest
-			LEFT JOIN rep ON rep.quest_id = quest.id
+			/*LEFT JOIN rep ON rep.quest_id = quest.id*/
 			JOIN utilisateur ON quest.user_id = utilisateur.id
 			WHERE quest.published = 1
 			ORDER BY dateCreated $direction 
